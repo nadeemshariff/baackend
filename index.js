e14368f3d1ea704b2aad129d28a28920fc949474
nadeemshariff <108820002+nadeemshariff@users.noreply.github.com>
2024-03-22 02:19:01 +0530

    express

---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+
+// Import the express module
+
+const express = require('express');
+
+// Create an instance of express
+const app = express();
+
+// Set the port number
+const port = 4000;
+
+// Define a route handler for the root path
+app.get('/', (req, res) => {
+  // Send a 'Hello World' message as the response
+  res.send('Hello World');
+});
+
+// Define a route handler for the /twitter path
+app.get('/twitter', (req, res) => {
+  // Send 'nadeem.com' as the response
+  res.send('nadeem.com');
+});
+
+app.get('/login', (req, res) => {
+    res.send("login");
+});
+
+app.get('/youtube', (req, res) => {
+    res.send('youtube.com');
+})
+
+// Start the server and listen on the specified port
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+// There is no need to call app.listen() twice with the same port number
+// The second call has been removed
